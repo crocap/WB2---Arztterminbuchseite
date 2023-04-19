acaa28435e30891a63cd2c145a3ef76534d55078
crocap <skforeverything@gmail.com>
2023-04-19 14:02:41 +0200

    datum richtig

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-window.addEventListener('load', function() {
+window.addEventListener('DOMContentLoaded', () => {
     var today = new Date();
     var yyyy = today.getFullYear();
-    var mm = String(today.getMonth() + 1).padStart(2, '0');
+    var mm = String(today.getMonth()+1).padStart(2, '0');
     var dd = String(today.getDate()).padStart(2, '0');
     var minDate = yyyy + '-' + mm + '-' + dd;
-
     document.getElementById("date").setAttribute("min", minDate);
     document.getElementById("date").setAttribute("value", minDate);
-  });
+});
