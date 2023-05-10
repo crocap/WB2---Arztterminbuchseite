ab61e7e753cd64d7773fb56814fe3e1f27e1bd5b
stutzma <130309681+stutzma@users.noreply.github.com>
2023-05-10 14:02:27 +0200

    Update geschlecht.js

--- Backend/services/geschlecht.js
+++ Backend/services/geschlecht.js
@@ -1,121 +1,90 @@
 const helper = require('../helper.js');
-const LandDao = require('../dao/geschlechtDao.js');
-const express = require('express');
-var serviceRouter = express.Router();
 
-console.log('- Service Land');
+class kontaktDao {
 
-serviceRouter.get('/land/gib/:id', function(request, response) {
-    console.log('Service Land: Client requested one record, id=' + request.params.id);
-
-    const landDao = new LandDao(request.app.locals.dbConnection);
-    try {
-        var obj = landDao.loadById(request.params.id);
-        console.log('Service Land: Record loaded');
-        response.status(200).json(obj);
-    } catch (ex) {
-        console.error('Service Land: Error loading record by id. Exception occured: ' + ex.message);
-        response.status(400).json({ 'fehler': true, 'nachricht': ex.message });
-    }
-});
-
-serviceRouter.get('/land/alle', function(request, response) {
-    console.log('Service Land: Client requested all records');
-
-    const landDao = new LandDao(request.app.locals.dbConnection);
-    try {
-        var arr = landDao.loadAll();
-        console.log('Service Land: Records loaded, count=' + arr.length);
-        response.status(200).json(arr);
-    } catch (ex) {
-        console.error('Service Land: Error loading all records. Exception occured: ' + ex.message);
-        response.status(400).json({ 'fehler': true, 'nachricht': ex.message });
-    }
-});
-
-serviceRouter.get('/land/existiert/:id', function(request, response) {
-    console.log('Service Land: Client requested check, if record exists, id=' + request.params.id);
-
-    console.log('go');
-
-    const landDao = new LandDao(request.app.locals.dbConnection);
-    try {
-        var exists = landDao.exists(request.params.id);
-        console.log('Service Land: Check if record exists by id=' + request.params.id + ', exists=' + exists);
-        response.status(200).json({'id': request.params.id, 'existiert': exists});
-    } catch (ex) {
-        console.error('Service Land: Error checking if record exists. Exception occured: ' + ex.message);
-        response.status(400).json({ 'fehler': true, 'nachricht': ex.message });
-    }
-});
-
-serviceRouter.post('/land', function(request, response) {
-    console.log('Service Land: Client requested creation of new record');
-
-    var errorMsgs=[];
-    if (helper.isUndefined(request.body.kennzeichnung)) 
-        errorMsgs.push('kennzeichnung fehlt');
-    if (helper.isUndefined(request.body.bezeichnung)) 
-        errorMsgs.push('bezeichnung fehlt');
-    
-    if (errorMsgs.length > 0) {
-        console.log('Service Land: Creation not possible, data missing');
-        response.status(400).json({ 'fehler': true, 'nachricht': 'Funktion nicht möglich. Fehlende Daten: ' + helper.concatArray(errorMsgs) });
-        return;
+    constructor(dbConnection) {
+        this._conn = dbConnection;
     }
 
-    const landDao = new LandDao(request.app.locals.dbConnection);
-    try {
-        var obj = landDao.create(request.body.kennzeichnung, request.body.bezeichnung);
-        console.log('Service Land: Record inserted');
-        response.status(200).json(obj);
-    } catch (ex) {
-        console.error('Service Land: Error creating new record. Exception occured: ' + ex.message);
-        response.status(400).json({ 'fehler': true, 'nachricht': ex.message });
-    }    
-});
-
-serviceRouter.put('/land', function(request, response) {
-    console.log('Service Land: Client requested update of existing record');
-
-    var errorMsgs=[];
-    if (helper.isUndefined(request.body.id)) 
-        errorMsgs.push('id fehlt');
-    if (helper.isUndefined(request.body.kennzeichnung)) 
-        errorMsgs.push('kennzeichnung fehlt');
-    if (helper.isUndefined(request.body.bezeichnung)) 
-        errorMsgs.push('bezeichnung fehlt');
-
-    if (errorMsgs.length > 0) {
-        console.log('Service Land: Update not possible, data missing');
-        response.status(400).json({ 'fehler': true, 'nachricht': 'Funktion nicht möglich. Fehlende Daten: ' + helper.concatArray(errorMsgs) });
-        return;
+    getConnection() {
+        return this._conn;
     }
 
-    const landDao = new LandDao(request.app.locals.dbConnection);
-    try {
-        var obj = landDao.update(request.body.id, request.body.kennzeichnung, request.body.bezeichnung);
-        console.log('Service Land: Record updated, id=' + request.body.id);
-        response.status(200).json(obj);
-    } catch (ex) {
-        console.error('Service Land: Error updating record by id. Exception occured: ' + ex.message);
-        response.status(400).json({ 'fehler': true, 'nachricht': ex.message });
-    }    
-});
+    loadById(id) {
+        var sql = 'SELECT * FROM Kontakt WHERE id=?';
+        var statement = this._conn.prepare(sql);
+        var result = statement.get(id);
 
-serviceRouter.delete('/land/:id', function(request, response) {
-    console.log('Service Land: Client requested deletion of record, id=' + request.params.id);
+        if (helper.isUndefined(result)) 
+            throw new Error('No Record found by id=' + id);
 
-    const landDao = new LandDao(request.app.locals.dbConnection);
-    try {
-        var obj = landDao.loadById(request.params.id);
-        landDao.delete(request.params.id);
-        console.log('Service Land: Deletion of record successfull, id=' + request.params.id);
-        response.status(200).json({ 'gelöscht': true, 'eintrag': obj });
-    } catch (ex) {
-        console.error('Service Land: Error deleting record. Exception occured: ' + ex.message);
-        response.status(400).json({ 'fehler': true, 'nachricht': ex.message });
+        return result;
     }
-});
 
-module.exports = serviceRouter;
+    loadAll() {
+        var sql = 'SELECT * FROM Kontakt';
+        var statement = this._conn.prepare(sql);
+        var result = statement.all();
+
+        if (helper.isArrayEmpty(result)) 
+            return [];
+        
+        return result;
+    }
+
+    exists(id) {
+        var sql = 'SELECT COUNT(id) AS cnt FROM Kontakt WHERE id=?';
+        var statement = this._conn.prepare(sql);
+        var result = statement.get(id);
+
+        if (result.cnt == 1) 
+            return true;
+
+        return false;
+    }
+
+    create(kennzeichnung = '', bezeichnung = '') {
+        var sql = 'INSERT INTO Kontakt (kennzeichnung,bezeichnung) VALUES (?,?)';
+        var statement = this._conn.prepare(sql);
+        var params = [kennzeichnung, bezeichnung];
+        var result = statement.run(params);
+
+        if (result.changes != 1) 
+            throw new Error('Could not insert new Record. Data: ' + params);
+
+        return this.loadById(result.lastInsertRowid);
+    }
+
+    update(id, kennzeichnung = '', bezeichnung = '') {
+        var sql = 'UPDATE Kontakt SET kennzeichnung=?,bezeichnung=? WHERE id=?';
+        var statement = this._conn.prepare(sql);
+        var params = [kennzeichnung, bezeichnung, id];
+        var result = statement.run(params);
+
+        if (result.changes != 1) 
+            throw new Error('Could not update existing Record. Data: ' + params);
+
+        return this.loadById(id);
+    }
+
+    delete(id) {
+        try {
+            var sql = 'DELETE FROM Kontakt WHERE id=?';
+            var statement = this._conn.prepare(sql);
+            var result = statement.run(id);
+
+            if (result.changes != 1) 
+                throw new Error('Could not delete Record by id=' + id);
+
+            return true;
+        } catch (ex) {
+            throw new Error('Could not delete Record by id=' + id + '. Reason: ' + ex.message);
+        }
+    }
+
+    toString() {
+        console.log('KontaktDao [_conn=' + this._conn + ']');
+    }
+}
+
+module.exports = kontaktDao;
