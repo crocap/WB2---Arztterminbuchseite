acfbf077457ad9f5c9c9ee010dca590a7271607b
stutzma <130309681+stutzma@users.noreply.github.com>
2023-05-10 14:05:32 +0200

    Update kontakt.js

--- Backend/services/kontakt.js
+++ Backend/services/kontakt.js
@@ -1,56 +1,58 @@
 const helper = require('../helper.js');
-const LandDao = require('../dao/kontaktDao.js');
+const kontaktDao = require('../dao/kontaktDao.js');
 const express = require('express');
 var serviceRouter = express.Router();
 
-console.log('- Service Land');
+console.log('- Service Kontakt');
 
-serviceRouter.get('/land/gib/:id', function(request, response) {
-    console.log('Service Land: Client requested one record, id=' + request.params.id);
 
-    const landDao = new LandDao(request.app.locals.dbConnection);
+serviceRouter.get('/kontakt/gib/:id', function(request, response) {
+    console.log('Service Kontakt: Client requested one record, id=' + request.params.id);
+
+    const kontaktDao = new kontaktDao(request.app.locals.dbConnection);
     try {
-        var obj = landDao.loadById(request.params.id);
-        console.log('Service Land: Record loaded');
+        var obj = kontaktDao.loadById(request.params.id);
+        console.log('Service Kontakt: Record loaded');
         response.status(200).json(obj);
     } catch (ex) {
-        console.error('Service Land: Error loading record by id. Exception occured: ' + ex.message);
+        console.error('Service Kontakt: Error loading record by id. Exception occured: ' + ex.message);
         response.status(400).json({ 'fehler': true, 'nachricht': ex.message });
     }
 });
 
-serviceRouter.get('/land/alle', function(request, response) {
-    console.log('Service Land: Client requested all records');
+serviceRouter.get('/kontakt/alle', function(request, response) {
+    console.log('Service Kontakt: Client requested all records');
 
-    const landDao = new LandDao(request.app.locals.dbConnection);
+    const kontaktDao = new kontaktDao(request.app.locals.dbConnection);
     try {
-        var arr = landDao.loadAll();
-        console.log('Service Land: Records loaded, count=' + arr.length);
+        var arr = kontaktDao.loadAll();
+        console.log('Service Kontakt: Records loaded, count=' + arr.length);
         response.status(200).json(arr);
     } catch (ex) {
-        console.error('Service Land: Error loading all records. Exception occured: ' + ex.message);
+        console.error('Service Kontakt: Error loading all records. Exception occured: ' + ex.message);
         response.status(400).json({ 'fehler': true, 'nachricht': ex.message });
     }
 });
 
-serviceRouter.get('/land/existiert/:id', function(request, response) {
-    console.log('Service Land: Client requested check, if record exists, id=' + request.params.id);
+serviceRouter.get('/kontakt/existiert/:id', function(request, response) {
+    console.log('Service Kontakt: Client requested check, if record exists, id=' + request.params.id);
 
     console.log('go');
 
-    const landDao = new LandDao(request.app.locals.dbConnection);
+    const kontaktDao = new kontaktDao(request.app.locals.dbConnection);
     try {
-        var exists = landDao.exists(request.params.id);
-        console.log('Service Land: Check if record exists by id=' + request.params.id + ', exists=' + exists);
+        var exists = kontaktDao.exists(request.params.id);
+        console.log('Service Kontakt: Check if record exists by id=' + request.params.id + ', exists=' + exists);
         response.status(200).json({'id': request.params.id, 'existiert': exists});
     } catch (ex) {
-        console.error('Service Land: Error checking if record exists. Exception occured: ' + ex.message);
+        console.error('Service Kontakt: Error checking if record exists. Exception occured: ' + ex.message);
         response.status(400).json({ 'fehler': true, 'nachricht': ex.message });
     }
 });
 
-serviceRouter.post('/land', function(request, response) {
-    console.log('Service Land: Client requested creation of new record');
+
+serviceRouter.post('/kontakt', function(request, response) {
+    console.log('Service Kontakt: Client requested creation of new record');
 
     var errorMsgs=[];
     if (helper.isUndefined(request.body.kennzeichnung)) 
@@ -59,24 +61,24 @@ serviceRouter.post('/land', function(request, response) {
         errorMsgs.push('bezeichnung fehlt');
     
     if (errorMsgs.length > 0) {
-        console.log('Service Land: Creation not possible, data missing');
+        console.log('Service Kontakt: Creation not possible, data missing');
         response.status(400).json({ 'fehler': true, 'nachricht': 'Funktion nicht möglich. Fehlende Daten: ' + helper.concatArray(errorMsgs) });
         return;
     }
 
-    const landDao = new LandDao(request.app.locals.dbConnection);
+    const kontaktDao = new kontaktDao(request.app.locals.dbConnection);
     try {
-        var obj = landDao.create(request.body.kennzeichnung, request.body.bezeichnung);
-        console.log('Service Land: Record inserted');
+        var obj = kontaktDao.create(request.body.kennzeichnung, request.body.bezeichnung);
+        console.log('Service Kontakt: Record inserted');
         response.status(200).json(obj);
     } catch (ex) {
-        console.error('Service Land: Error creating new record. Exception occured: ' + ex.message);
+        console.error('Service Kontakt: Error creating new record. Exception occured: ' + ex.message);
         response.status(400).json({ 'fehler': true, 'nachricht': ex.message });
     }    
 });
 
-serviceRouter.put('/land', function(request, response) {
-    console.log('Service Land: Client requested update of existing record');
+serviceRouter.put('/kontakt', function(request, response) {
+    console.log('Service Kontakt: Client requested update of existing record');
 
     var errorMsgs=[];
     if (helper.isUndefined(request.body.id)) 
@@ -87,33 +89,33 @@ serviceRouter.put('/land', function(request, response) {
         errorMsgs.push('bezeichnung fehlt');
 
     if (errorMsgs.length > 0) {
-        console.log('Service Land: Update not possible, data missing');
+        console.log('Service Kontakt: Update not possible, data missing');
         response.status(400).json({ 'fehler': true, 'nachricht': 'Funktion nicht möglich. Fehlende Daten: ' + helper.concatArray(errorMsgs) });
         return;
     }
 
-    const landDao = new LandDao(request.app.locals.dbConnection);
+    const kontaktDao = new kontaktDao(request.app.locals.dbConnection);
     try {
-        var obj = landDao.update(request.body.id, request.body.kennzeichnung, request.body.bezeichnung);
-        console.log('Service Land: Record updated, id=' + request.body.id);
+        var obj = kontaktDao.update(request.body.id, request.body.kennzeichnung, request.body.bezeichnung);
+        console.log('Service Kontakt: Record updated, id=' + request.body.id);
         response.status(200).json(obj);
     } catch (ex) {
-        console.error('Service Land: Error updating record by id. Exception occured: ' + ex.message);
+        console.error('Service Kontakt: Error updating record by id. Exception occured: ' + ex.message);
         response.status(400).json({ 'fehler': true, 'nachricht': ex.message });
     }    
 });
 
-serviceRouter.delete('/land/:id', function(request, response) {
-    console.log('Service Land: Client requested deletion of record, id=' + request.params.id);
+serviceRouter.delete('/kontakt/:id', function(request, response) {
+    console.log('Service Kontakt: Client requested deletion of record, id=' + request.params.id);
 
-    const landDao = new LandDao(request.app.locals.dbConnection);
+    const kontaktDao = new kontaktDao(request.app.locals.dbConnection);
     try {
-        var obj = landDao.loadById(request.params.id);
-        landDao.delete(request.params.id);
-        console.log('Service Land: Deletion of record successfull, id=' + request.params.id);
+        var obj = kontaktDao.loadById(request.params.id);
+        kontaktDao.delete(request.params.id);
+        console.log('Service Kontakt: Deletion of record successfull, id=' + request.params.id);
         response.status(200).json({ 'gelöscht': true, 'eintrag': obj });
     } catch (ex) {
-        console.error('Service Land: Error deleting record. Exception occured: ' + ex.message);
+        console.error('Service Kontakt: Error deleting record. Exception occured: ' + ex.message);
         response.status(400).json({ 'fehler': true, 'nachricht': ex.message });
     }
 });
