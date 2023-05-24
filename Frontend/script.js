da66a5200abe71baa943f749c4f9108aff554014
crocap <skforeverything@gmail.com>
2023-05-24 16:54:09 +0200

    script.js gelöscht, weil nicht benutzt

--- Frontend/script.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('DOMContentLoaded', () => {
-    var today = new Date();
-    var yyyy = today.getFullYear();
-    var mm = String(today.getMonth()+1).padStart(2, '0');
-    var dd = String(today.getDate()).padStart(2, '0');
-    var minDate = yyyy + '-' + mm + '-' + dd;
-    document.getElementById("date").setAttribute("min", minDate);
-    document.getElementById("date").setAttribute("value", minDate);
-});
